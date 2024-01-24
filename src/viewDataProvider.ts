1a35c6d6d158a78379ddad8f28e91016c356ed3b
Michael Jolley <mike@baldbeardedbuilder.com>
2024-01-24 16:09:48 -0600

    Continuing to parse file tree

--- src/viewDataProvider.ts
+++ src/viewDataProvider.ts
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import * as vscode from "vscode";
 
-import { BaseTreeItem } from "./baseTreeItem";
-import { BaseTreeViewDataProvider } from "./baseTreeViewDataProvider";
+const componentRegex = /.*\.(tsx)|(jsx)|(ts)|(js)|(vue)/g;
+const componentGlob = "**/*.{ts,tsx,js,jsx,vue}";
 
 export class CompTreeItem extends vscode.TreeItem {
   parent: CompTreeItem | undefined;
   children: CompTreeItem[] = [];
   path: string = "";
+  isFolder: boolean = false;
 
   constructor(
     label: string,
@@ -16,26 +17,41 @@ export class CompTreeItem extends vscode.TreeItem {
       .TreeItemCollapsibleState.None,
     icon: vscode.ThemeIcon = vscode.ThemeIcon.Folder,
     command?: vscode.Command,
-    contextValue = "cleancode",
+    isFolder?: boolean = false
   ) {
-    super(label, collapsibleState);
+    super(label.replace("/", ""), collapsibleState);
     this.command = command;
     this.iconPath = icon;
     this.path = path;
-    this.contextValue = contextValue;
+    this.isFolder = isFolder;
   }
 }
 
 export class ViewDataProvider implements vscode.TreeDataProvider<CompTreeItem> {
   private treeItems: CompTreeItem[] | null = null;
   private _onDidChangeTreeData: vscode.EventEmitter<CompTreeItem | null> =
-    new vscode.EventEmitter<CompTreeItem | null>();
+   new vscode.EventEmitter<CompTreeItem | null>();
   readonly onDidChangeTreeData: vscode.Event<CompTreeItem | null> =
-    this._onDidChangeTreeData.event;
+     this._onDidChangeTreeData.event;
+
+  private _fsWatcher: vscode.FileSystemWatcher | undefined;
+
+  constructor() {
+    this._fsWatcher = vscode.workspace.createFileSystemWatcher(componentGlob);
+
+    this._fsWatcher.onDidChange((event) => this.fileChanged(event));
+    this._fsWatcher.onDidCreate((event) => this.fileChanged(event));
+    this._fsWatcher.onDidDelete((event) => this.fileChanged(event));
+
+    this.buildTree();
+  }
+
+  dispose() {
+    this._fsWatcher?.dispose();
+  }
 
   public refresh() {
     this.treeItems = null;
-    this._onDidChangeTreeData.fire(null);
   }
 
   public getTreeItem(element: CompTreeItem): CompTreeItem {
@@ -43,7 +59,7 @@ export class ViewDataProvider implements vscode.TreeDataProvider<CompTreeItem> {
   }
 
   public getParent(element: CompTreeItem): CompTreeItem | null {
-    if (element instanceof BaseTreeItem && element.parent) {
+    if (element instanceof CompTreeItem && element.parent) {
       return element.parent;
     }
     return null;
@@ -66,7 +82,30 @@ export class ViewDataProvider implements vscode.TreeDataProvider<CompTreeItem> {
     return [];
   }
 
-  async buildTree(): Promise<CompTreeItem[]> {
+  private fileChanged(event: vscode.Uri) {
+    if (event.path.match(componentRegex) && this.treeItems) {
+      const lastSlash = event.path.lastIndexOf("/");
+      const filename = event.path.slice(lastSlash);
+      const path = event.path.slice(0, lastSlash);
+
+      const parent = this.treeItems.find((c) => c.path.toLocaleLowerCase() === path.toLocaleLowerCase());
+      if (parent) {
+        parent.children.push(
+          new CompTreeItem(
+            filename,
+            event.path,
+            undefined,
+            vscode.ThemeIcon.File,
+          ),
+        );
+        parent.children = parent.children.sort((a, b) => a.path.localeCompare(b.path));
+      }
+    }
+
+    this._onDidChangeTreeData.fire(null);
+  }
+
+  private async buildTree(): Promise<CompTreeItem[]> {
     if (!vscode.workspace.workspaceFolders) {
       return [];
     }
@@ -81,19 +120,31 @@ export class ViewDataProvider implements vscode.TreeDataProvider<CompTreeItem> {
       );
 
       let files = await vscode.workspace.findFiles(
-        `**/*.md`,
+        componentGlob,
         "**/node_modules/**",
       );
 
       if (files) {
+
+        files = files.sort(this.treeSort);
+
         for (const file of files) {
           const lastSlash = file.path.lastIndexOf("/");
           const filename = file.path.slice(lastSlash);
           const path = file.path.slice(0, lastSlash);
 
           const parent = trunk.children.find((c) => c.path === path);
-
-          if (parent) {
+          if (folder.uri.path.toLocaleLowerCase() === path.toLocaleLowerCase()) {
+            trunk.children.push(
+              new CompTreeItem(
+                filename,
+                file.path,
+                undefined,
+                vscode.ThemeIcon.File,
+              ),
+            );
+          }
+          else if (parent) {
             parent.children.push(
               new CompTreeItem(
                 filename,
@@ -125,5 +176,9 @@ export class ViewDataProvider implements vscode.TreeDataProvider<CompTreeItem> {
     }
 
     return items;
+  }
+
+  private treeSort(a: CompTreeItem, b: CompTreeItem): number {
+    return a.isFolder > b.isFolder && a.path.localeCompare(b.path) ? 1 : -1;
   }
 }
